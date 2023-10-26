6e64a3303b074fcc79c06d99d6fcb018d3c2eee1
nobody <github2@invisiblehand.church>
2023-10-26 17:08:54 -0400

    x

--- addr.ts
+++ addr.ts
@@ -1,32 +1,45 @@
 import fs from 'fs';
 import {hash160} from '@cashscript/utils';
-import {deriveHdPath, deriveHdPrivateNodeFromSeed, encodeCashAddress, secp256k1,} from '@bitauth/libauth';
+import {deriveHdPath, deriveHdPrivateNodeFromSeed, encodeCashAddress ,} from '@bitauth/libauth';
 import {mnemonicToSeed} from 'bip39';
 import fetch from 'node-fetch';
 import {writeFileSync} from "fs";
 // Generate entropy from BIP39 mnemonic phrase and initialise a root
 // HD-wallet node
-class Wallet {
-}
 import { mnemonic } from './calcMnemonic.js';
+console.log("loaded mnemonic");
 const seed = await mnemonicToSeed(mnemonic.join(" "));
-function readJson(name) {
-  const buf = fs.readFileSync(name);
-  const str = buf.toString();
-  const obj = JSON.parse(str);
-  console.log(obj);
-  return obj;
-}
+// function readJson(name) {
+//   const buf = fs.readFileSync(name);
+//   const str = buf.toString();
+//   const obj = JSON.parse(str);
+//   console.log(obj);
+//   return obj;
+// }
 const derivationBase = "m/44'/145'/0'/0/";
 const prefix= 'bitcoincash';
 const addrType = 'p2pkh';
+
+function u8a(maybeString) {
+  if (maybeString instanceof Uint8Array) {
+    return maybeString;
+  } else if (typeof (maybeString) === 'string') {
+    const res = new Uint8Array(maybeString.length);
+    new TextEncoder().encodeInto(maybeString, res);
+    return res;
+  } else {
+    return u8a(maybeString.toString());
+  }
+}
+
 function derive(path, addrType, prefix) {
   const rootNode= deriveHdPrivateNodeFromSeed(seed, true);
   const aliceNode = deriveHdPath(rootNode, path);
-  const alicePub = secp256k1.derivePublicKeyCompressed(aliceNode.privateKey);
+  const privateKey = u8a(aliceNode.privateKey);
+  const alicePub =
+      usecp256k1.derivePublicKeyCompressed(privateKey);
   const alicePkh = hash160(alicePub);
-  const aliceAddr = encodeCashAddress(prefix, addrType, alicePkh);
-  return aliceAddr;
+  return encodeCashAddress(prefix, addrType, alicePkh);
 }
 const addrs=[];
 for(let i=0;i<20;i++) {
