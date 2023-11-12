204010f2a3aa27fcfac87461c7855a1b25999e85
nobody <github2@invisiblehand.church>
2023-11-12 18:14:54 -0500

    x

--- mnemonic.ts
+++
@@ -1,4 +0,0 @@
-export const mnemonic: string = [
-  "abandon",  "abandon",  "abandon",  "abandon",  "abandon",  "abandon",
-  "abandon",  "abandon",  "abandon",  "abandon",  "abandon",  "about"
-].join(" ");
