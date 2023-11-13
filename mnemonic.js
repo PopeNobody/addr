0dcacbf3c53611c1718f097265fc6bf9363df7e5
nobody <github2@invisiblehand.church>
2023-11-13 01:49:23 -0500

    added orig sample code

--- mnemonic.js
+++ mnemonic.js
@@ -1,4 +1,5 @@
-export const mnemonic = [
-    "abandon", "abandon", "abandon", "abandon", "abandon", "abandon",
-    "abandon", "abandon", "abandon", "abandon", "abandon", "about"
-].join(" ");
+import fs from 'fs';
+
+const text = fs.readFileSync("mnemonic.txt").toString();
+const words = text.split(/\s+/);
+export const mnemonic = words.join(" ");
