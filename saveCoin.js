b27b89590052890c64d8a2fde09bd3aeb14d8759
nobody <github2@invisiblehand.church>
2023-11-12 19:26:55 -0500

    moved test and regtest into dir

--- saveCoin.js
+++ saveCoin.js
@@ -3,5 +3,12 @@ global.saveCoin=saveCoin;
 console.log({saveCoin});
 function saveCoin(coin,net,obj) {
     const json = JSON.stringify(obj,null,2);
-    writeFileSync(`./chains/${coin}.${net}.json`,json);
+    if(net==="main") {
+        net="";
+    } else {
+        net=net+"/";
+    }
+    const file = `./chains/${net}${coin}.json`;
+    console.log({coin,net,file});
+    writeFileSync(file,json);
 }
