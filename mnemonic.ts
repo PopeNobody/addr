fca4b4fab5738f092e5b96717278df68cb189bce
nobody <github2@invisiblehand.church>
2023-11-14 07:12:04 -0500

    x

--- mnemonic.ts
+++
@@ -1,8 +0,0 @@
-import {readFileSync} from 'fs';
-
-let mn = readFileSync("mnemonic.txt").toString();
-mn=mn.trim();
-const ar = mn.split(/\s+/);
-if(mn.length % 3)
-    throw new Error("mnemonic phrase length should be multiple of 3")
-export const mnemonic = ar.join(" ");
